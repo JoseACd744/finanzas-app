5703fc9e8bf38add5cd6e0ae95fb606f7291dc6c
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-12 23:11:01 -0500

    Actualizar URLs de la API a HTTPS en varios servicios y mejorar la lógica de carga en la vista de perfil de usuario

--- src/app/services/letra.service.ts
+++ src/app/services/letra.service.ts
@@ -7,7 +7,7 @@ import { Letra } from '../models/letra.model';
   providedIn: 'root'
 })
 export class LetraService {
-  private apiUrl = 'http://finanzastf-h0hgfnerg9eca4ba.westus3-01.azurewebsites.net/api/letras'; // Cambia esto a tu URL de API
+  private apiUrl = 'https://finanzastf-h0hgfnerg9eca4ba.westus3-01.azurewebsites.net/api/letras'; // Cambia esto a tu URL de API
 
   constructor(private http: HttpClient) {}
 
