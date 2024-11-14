bf66bebbeae1c4363270e26edb928bdc8c5f902f
José Anthony Arenas Conde <u20211d744@upc.edu.pe>
2024-11-14 17:32:13 -0500

    Añadir columnas para nombre de la entidad financiera y fecha de emisión en la vista de historial, junto con actualizaciones en el modelo y detalles del PDF

--- src/app/models/letra.model.ts
+++ src/app/models/letra.model.ts
@@ -1,23 +1,25 @@
 // src/app/models/letra.model.ts
 export interface Letra {
-    id?: number;
-    numero: string;
-    nombreCliente: string;
-    monto: number;
-    tasaInteresEfectiva: number;
-    seguroDesgravame: number;
-    fechaDescuento: Date;
-    fechaVencimiento: Date;
-    fechaInicio?: Date;
-    valorNominal?: number;
-    diasDescontados?: number;
-    TEA?: number;
-    TEP?: number;
-    tasaDescontada?: number;
-    descuento?: number;
-    valorNeto?: number;
-    valorRecibido?: number;
-    valorEntregado?: number;
-    TCEA?: number;
-    userId: number;
-  }
+  id?: number;
+  numero: string;
+  nombreCliente: string;
+  nombreEntidadFinanciera: string; // Nuevo campo añadido
+  monto: number;
+  fechaEmision: Date; // Nuevo campo añadido
+  tasaInteresEfectiva: number;
+  seguroDesgravame: number;
+  fechaDescuento: Date;
+  fechaVencimiento: Date;
+  fechaInicio?: Date;
+  valorNominal?: number;
+  diasDescontados?: number;
+  TEA?: number;
+  TEP?: number;
+  tasaDescontada?: number;
+  descuento?: number;
+  valorNeto?: number;
+  valorRecibido?: number;
+  valorEntregado?: number;
+  TCEA?: number;
+  userId: number;
+}
